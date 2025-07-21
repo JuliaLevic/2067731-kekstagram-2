83a5fbc6d490f5d425d97660615c219934374a55
JuliaLevic <julialevic@yandex.ru>
2025-07-21 16:41:16 +0300

    изменяет модуляцию 3

--- js/createArrayPhotos.js
+++ js/createArrayPhotos.js
@@ -8,13 +8,23 @@ const MIN_COMMENTS = 0;
 const MAX_COMMENTS = 30;
 const MIN_NUMBER_AVATAR = 1;
 const MAX_NUMBER_AVATAR = 6;
-const MIN_ID_COMMENT=1;
-const MAX_ID_COMMENT=1000;
+const MIN_ID_COMMENT = 1;
+const MAX_ID_COMMENT = 1000;
+const MIN_ID_PHOTO = 1;
+const MAX_ID_PHOTO = 25;
+const MIN_NUMBER_PHOTO=1;
+const MAX_NUMBER_PHOTO=25;
 const { NAMES, DESCRIPTIONS, MESSAGES } = getDataArrayPhotos();
 
 
 //Id комментария
-const getIdComment=getUnicalInteger(MIN_ID_COMMENT,MAX_ID_COMMENT,)
+const getIdComment = getUnicalInteger(MIN_ID_COMMENT, MAX_ID_COMMENT,)
+
+//Id фото
+const getIdPhoto = getUnicalInteger(MIN_ID_PHOTO, MAX_ID_PHOTO,)
+
+//Номер фото
+const getNumberPhoto = getUnicalInteger(MIN_NUMBER_PHOTO, MAX_NUMBER_PHOTO,)
 
 
 //Объект комментатора
@@ -28,15 +38,15 @@ const createComment = () => ({
 
 //Объект фотографии
 
-const createPhoto = (id) => ({
-  id: id,
-  url: `photos/${id}.jpg`,
+const createPhoto = () => ({
+  id: getIdPhoto(),
+  url: `photos/${getNumberPhoto()}.jpg`,
   description: getRandomElement(DESCRIPTIONS),
   likes: getRandomInteger(MIN_LIKES, MAX_LIKES),
-  comments: Array.from({ length: getRandomInteger(MIN_COMMENTS,MAX_COMMENTS) }, createComment),
+  comments: Array.from({ length: getRandomInteger(MIN_COMMENTS, MAX_COMMENTS) }, createComment),
 });
 
 //Создаем массив
-const createArrayPhotos = () => Array.from({ length: MAX_OBJECT }, (v, i) => createPhoto(i + 1));
+const createArrayPhotos = () => Array.from({ length: MAX_OBJECT }, createPhoto);
 
 export { createArrayPhotos };
